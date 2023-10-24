4c04a7678cfd2bf4b5202b7cdf73ff0b003731f6
beastly-eve <78984411+beastly-eve@users.noreply.github.com>
2023-10-24 13:45:37 -0700

    Updated comments in config script

--- themes/theme-name-here/theme-name-here.gd
+++ themes/theme-name-here/theme-name-here.gd
@@ -1,8 +1,8 @@
 extends Control
 
-export(String) var theme_name = "Your Theme Name Here"
+export(String) var theme_name = "Your Theme Name Here" # the human readable name of your theme that will be shown in the dropdown
 export(String) var theme_author = "Author Name Here"
-export(String) var theme_version = "1.0"
+export(String) var theme_version = "1.0" 
 export(String) var id = "theme-id-here" # Theme ID must match the root node name in theme.tscn, use a combination of author name and theme name. So a Speed Metal theme by Heavy Dude would have an id of heavy-dude-speed-metal
 export(String) var folder_name = "theme-name-here" # Replace with your alpha-numeric theme name, use dashes instead of space (kebab case)
 export(String) var pack = "theme-pack-name-here" # If you export multiple themes together they must share a pack name, include a pack name even if your theme is solo
